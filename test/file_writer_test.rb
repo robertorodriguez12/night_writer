4db1e9a52d68e275f0eb567abf1425a6a5fde17e
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-31 17:56:08 -0600

    Create test it exists test

--- test/file_writer_test.rb
+++ test/file_writer_test.rb
@@ -8,9 +8,18 @@ class FileWriterTest < MiniTest::Test
 
   def test_it_exists
     file_writer = FileWriter.new
-
     assert_instance_of FileWriter, file_writer
   end
 
+  def test
+
+  end
+
+  # def test_it_can_write
+  #   render_to
+  # end
 
 end
+
+
+#add test in here for braille_keys module

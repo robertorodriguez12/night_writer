f2a3c82033587e106909f25765224a59cab77ea1
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-31 17:23:09 -0600

    Create file writer class

--- lib/file_writer.rb
+++ lib/file_writer.rb
@@ -1,14 +1,10 @@
+require './lib/translator'
+
 class FileWriter
 
-  def initialzie
-    @translated_message = ARGV[1]
-    @char_count         = 0
+  def initialize
+    @translator = Translator.new
   end
 
-  def write_braille_message
-    if @translated_message == "braille.txt"
-      @char_count = File.write
-
-  end
 
 end

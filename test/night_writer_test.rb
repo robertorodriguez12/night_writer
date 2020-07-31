23c50d9110932bb586eded2a9378d91342366514
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-31 15:23:40 -0600

    Add instance of test

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,14 +1,27 @@
 require 'minitest/autorun'
 require 'minitest/nyan_cat'
-require './lib/night_writer'
+require './lib/n_w_class'
 require 'pry'
 
 class NightWriterTest < MiniTest::Test
 
-  def test_it_exists_and_attributes
-    night_writer = NightWriter.new
+  def test_it_exists
+    message = "message.txt"
+    render_to = "braille.txt"
+    # binding.pry
+    ARGV[0] = message
+    ARGV[1] = render_to
+    night_writer = NightWriter.new(message, render_to)
+
     assert_instance_of NightWriter, night_writer
-    assert_instance_of FileReader, night_writer.reader
   end
+
+  # def test_it_can_read_file
+  #   message = "message.txt"
+  #   night_writer = NightWriter.new
+  #
+  #   expected = "hello world"
+  #   assert_equal expected, night_writer.read
+  # end
 
 end

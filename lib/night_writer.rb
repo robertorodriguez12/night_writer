d0aad434ccf6fe3174612da60f3e5ba611fb09b2
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-30 20:37:23 -0600

    Initialized with filereader

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,11 +1,24 @@
+require './lib/file_reader'
 class NightWriter
 
-  def initialize(argument)
-    @argument = argument
+  attr_reader :reader
+
+  def initialize
+    @reader = FileReader.new
   end
 
-  def read
+  # def encode_file_to_braille
+  #   # I wouldn't worry about testing this method
+  #   # unless you get everything else done
+  #   plain = reader.read
+  #   braille = encode_to_braille(plain)
+  # end
 
+  def encode_to_braille(input)
+    # you've taken in an INPUT string
+    # do the magic
+    # send out an OUTPUT string
   end
-
 end
+
+puts ARGV.inspect

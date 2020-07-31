ff9ed15309232a40da0b3dcc4a62a053bafa5159
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-31 17:01:58 -0600

    Get rid of commented out code

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -5,12 +5,3 @@ class FileReader
     File.read(filename)
   end
 end
-
-
-
-# class FileReader
-#   def read_file(message)
-#     reader = File.open(message)
-#     @file_content = reader.read.chomp
-#   end
-# end

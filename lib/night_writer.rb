946a84dc92f8c1cba8725ac23f1d6254144322e8
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-31 15:24:03 -0600

    Pulled night_writer class out of here and made this a runner file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,34 +1,3 @@
-require './lib/file_reader'
-require 'pry'
-class NightWriter
+require './lib/n_w_class'
 
-  attr_reader :file_reader
-  attr_accessor :message
-
-  def initialize(message, render_to)
-    @reader    = FileReader.new
-    @message   = @reader.read_file(message)
-    @render_to = render_to
-    initial_input
-  end
-
-  # def encode_file_to_braille
-  #   # I wouldn't worry about testing this method
-  #   # unless you get everything else done
-  #   plain = reader.read
-  #   braille = encode_to_braille(plain)
-  # end
-
-  # def encode_to_braille(input)
-  #   # you've taken in an INPUT string
-  #   # do the magic
-  #   # send out an OUTPUT string
-  # end
-
-  def initial_input
-    puts "Created '#{@render_to}' containing #{@message.length} characters"
-  end
-end
-
-
-night_writer = NightWriter.new(ARGV[0], ARGV[1])
+NightWriter.new(ARGV[0], ARGV[1])

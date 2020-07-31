f7fc92c65919f979d9ffd903c030ce7b77cd9679
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-31 15:23:04 -0600

    Refactor to use method shown in interaction pattern

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -1,7 +1,16 @@
 require 'pry'
 class FileReader
-  def read_file(message)
-    reader = File.open(message)
-    @file_content = reader.read.chomp
+  def read
+    filename = ARGV[0]
+    File.read(filename)
   end
 end
+
+
+
+# class FileReader
+#   def read_file(message)
+#     reader = File.open(message)
+#     @file_content = reader.read.chomp
+#   end
+# end

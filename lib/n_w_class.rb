433c033719eb6c9e0b8d8b16e1adf403f3db5c84
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-08-01 11:41:15 -0600

    Create method that incorporates translation method and creates a new file with translation in it

--- lib/n_w_class.rb
+++ lib/n_w_class.rb
@@ -1,28 +1,29 @@
 require './lib/file_reader'
+require './lib/translator'
 # require './lib/file_writer'
 require 'pry'
 
 class NightWriter
 
-  attr_reader :message, :writer
-
   def initialize(message, render_to)
-    @reader    = FileReader.new
-    # @writer    = FileWriter.new
-    @message   = @reader.read
-    @render_to = render_to
+    @reader     = FileReader.new
+    @message    = @reader.read
+    @translator = Translator.new
+    @render_to  = render_to
     initial_input
+    translate_to_braille_and_create_new_file
   end
 
-  # def encode_file_to_braille
-  #   add method from translator class
-  # end
+  def translate_to_braille_and_create_new_file
+    message = @message.gsub(" ","")
+    translated = @translator.translation(message)
+    File.write(@render_to, translated)
+  end
 
   #def create method in here to eventually translate to english
 
   def initial_input
     puts "Created '#{@render_to}' containing #{@message.length} characters"
-    File.write(@render_to, File.read(ARGV[0]))
   end
 
 end

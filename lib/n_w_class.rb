5dc59ec7fcd5f1eee02ab9d1f0e6d0e2b8066b94
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-31 19:32:26 -0600

    Remove bry

--- lib/n_w_class.rb
+++ lib/n_w_class.rb
@@ -1,28 +1,23 @@
 require './lib/file_reader'
+require './lib/file_writer'
 require 'pry'
 
 class NightWriter
 
+  attr_reader :message, :writer
+
   def initialize(message, render_to)
     @reader    = FileReader.new
-    # binding.pry
+    @writer    = FileWriter.new
     @message   = @reader.read
     @render_to = render_to
     initial_input
   end
 
-  # def encode_file_to_braille
-  #   # I wouldn't worry about testing this method
-  #   # unless you get everything else done
-  #   plain = reader.read
-  #   braille = encode_to_braille(plain)
-  # end
+  def encode_file_to_braille
+    @writer.encode_to_braille(@message)
+  end
 
-  # def encode_to_braille(input)
-  #   # you've taken in an INPUT string
-  #   # do the magic
-  #   # send out an OUTPUT string
-  # end
 
   def initial_input
     puts "Created '#{@render_to}' containing #{@message.length} characters"

abbb996a37b00d990652efcc4c66bcdc50aea9c0
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-31 21:19:11 -0600

    Add methods to transpose and translate to braille

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,6 +1,30 @@
-require './lib/braille_keys'
+require_relative 'braille_keys'
+require 'pry'
 
 class Translator
+  include BrailleKeys
+# binding.pry
+  def translation(message)
+    split      = split_characters(message)
+    braille    = translate_to_braille(split)
+    transposed = transpose(braille)
+    strings    = create_strings(transposed)
+    format_lines(strings)
+  end
 
+  def split_characters(message)
+    # binding.pry
+    message.chars
+  end
+
+  def translate_to_braille(split)
+    split.map do |letter|
+      braille_keys[letter]
+    end
+  end
+
+  def transpose(braille)
+    braille.transpose
+  end
 
 end

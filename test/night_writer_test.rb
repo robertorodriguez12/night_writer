8a1206486e1056639bdf86e625ad886152709543
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-31 19:33:05 -0600

    Remove empty lines

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,3 +1,4 @@
+require './test/test_helper'
 require 'minitest/autorun'
 require 'minitest/nyan_cat'
 require './lib/n_w_class'
@@ -8,20 +9,10 @@ class NightWriterTest < MiniTest::Test
   def test_it_exists
     message = "message.txt"
     render_to = "braille.txt"
-    # binding.pry
     ARGV[0] = message
     ARGV[1] = render_to
     night_writer = NightWriter.new(message, render_to)
 
     assert_instance_of NightWriter, night_writer
   end
-
-  # def test_it_can_read_file
-  #   message = "message.txt"
-  #   night_writer = NightWriter.new
-  #
-  #   expected = "hello world"
-  #   assert_equal expected, night_writer.read
-  # end
-
 end

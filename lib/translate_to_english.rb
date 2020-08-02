48090fd59188f13a8ec6d5034808ce3ea01dc35e
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-08-01 18:58:26 -0600

    Add methods to get first and second elements of braille structure from a message

--- lib/translate_to_english.rb
+++ lib/translate_to_english.rb
@@ -11,6 +11,14 @@ class BrailleToEnglish
   def split_braille(message)
     length = message.length / 3
     message.scan(/.{1,#{length}}/)
-    # binding.pry
   end
+
+  def get_first_braille_element(input)
+    input.fetch(0).scan(/.{1,2}/)
+  end
+
+  def get_second_braille_element(input)
+    input.fetch(1).scan(/.{1,2}/)
+  end
+
 end

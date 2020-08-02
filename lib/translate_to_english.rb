4523b2819644bdfcba3d2c074ae699fb7a47fa3f
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-08-01 18:14:17 -0600

    Do some initial set up

--- lib/translate_to_english.rb
+++ lib/translate_to_english.rb
@@ -2,7 +2,14 @@ require_relative 'english_keys'
 require 'pry'
 
 class BrailleToEnglish
+  include EnglishKeys
 
-  
+  def initialize
+    @argument = argument
+  end
+
+  def translation(message)
+
+  end
 
 end

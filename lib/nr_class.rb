2715168ba44aa839bc77c59f2de119127bd29717
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-08-01 21:10:04 -0600

    Finish iteration 3

--- lib/nr_class.rb
+++ lib/nr_class.rb
@@ -1,5 +1,5 @@
 require './lib/file_reader'
-require './lib/translator'
+require './lib/translate_to_english'
 require 'pry'
 
 class NightReader
@@ -7,12 +7,17 @@ class NightReader
   def initialize(message, render_to)
     @reader     = FileReader.new
     @message    = @reader.read
-    @translator = Translator.new
+    @translator = BrailleToEnglish.new
     @render_to  = render_to
     confirmation_output
+    translate_to_english_and_create_new_file
   end
 
-
+  def translate_to_english_and_create_new_file
+    message = @message
+    translated = @translator.translation(message)
+    File.write(@render_to, translated)
+  end
 
   def confirmation_output
     puts "Created '#{@render_to}' containing #{@message.length} characters"
